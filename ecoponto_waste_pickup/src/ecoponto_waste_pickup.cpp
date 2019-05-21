f91b9d717ea9808fb8734175492506930535cc54
Marantesss <up201706917@fe.up.pt>
2019-05-21 11:48:38 +0100

    Added stuff to work with graphviewer + maps

--- ecoponto_waste_pickup/src/ecoponto_waste_pickup.cpp
+++ ecoponto_waste_pickup/src/ecoponto_waste_pickup.cpp
@@ -1,9 +1,29 @@
+#include <cstdio>
+#include "graphviewer.h"
+#include <fstream>
 #include <iostream>
+#include <sstream>
+
 
 using namespace std;
 
 int main() {
 	cout << "EcoPonto - Selective Waste Pickup!!!" << endl;
 	cout << "Under Construction!!!" << endl;
+
+	GraphViewer *gv = new GraphViewer(600, 600, true);
+	gv->createWindow(600, 600);
+	gv->defineVertexColor("blue");
+	gv->defineEdgeColor("black");
+	gv->addNode(1);
+	gv->addNode(2);
+	gv->addNode(3);
+	gv->rearrange();
+	// para arestas bidireccionais
+	gv->addEdge(1,1,2,EdgeType::UNDIRECTED);
+	// para arestas direccionais
+	gv->addEdge(2,1,3, EdgeType::DIRECTED);
+	gv->setVertexLabel(1, "Isto e um no");
+
 	return 0;
 }
